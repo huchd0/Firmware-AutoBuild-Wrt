38818dc16ac625824ec3217b2cc2d7060424a814
huchd0 <162666259+huchd0@users.noreply.github.com>
2026-05-23 19:52:30 +0800

    Update immortalwrt-build-x86-64-Deluxe.sh

--- x86-64/immortalwrt-build-x86-64-Deluxe.sh
+++ x86-64/immortalwrt-build-x86-64-Deluxe.sh
@@ -362,7 +362,6 @@ echo "=== 5. 配置 ImmortalWrt 专属软件列表 ==="
 # --- 1. 核心系统与基础 UI ---
 PKG_CORE=(
     "-dnsmasq"                         # 排除基础版 dnsmasq
-    "-dnsmasq-default"                 # 排除默认 dnsmasq 配置
     "dnsmasq-full"                     # 替换为全功能版（科学上网、透明代理必需支持）
     "luci"                             # 路由器 Web 管理后台主程序
     "luci-base"                        # Luci 基础依赖
